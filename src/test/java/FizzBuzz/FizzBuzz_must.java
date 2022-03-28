920c20573ecd1ae42b1402e7a6b12c8410ff7ce1
Pablo <alu0101225364@ull.edu.es>
2022-03-28 17:14:03 +0100

    first test in red

--- src/test/java/FizzBuzz/FizzBuzz_must.java
+++ src/test/java/FizzBuzz/FizzBuzz_must.java
@@ -2,6 +2,17 @@ package FizzBuzz;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzz_must {
 
+    @Test
+    void show_numbers_as_String(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals(fizzBuzz.play(1), "1");
+        assertEquals(fizzBuzz.play(2), "2");
+        assertEquals(fizzBuzz.play(4), "4");
+        assertEquals(fizzBuzz.play(98), "98");
+    }
 }
