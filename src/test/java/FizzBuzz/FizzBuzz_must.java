939a73ec50d2ea987f4d649406482cbf90522903
Pablo <alu0101225364@ull.edu.es>
2022-03-28 16:29:34 +0100

    Changes by pablo

--- src/test/java/FizzBuzz/FizzBuzz_must.java
+++ src/test/java/FizzBuzz/FizzBuzz_must.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzz_must {
     @Test
-    public void returns_array_of_numbers_as_array_of_strings_with_numbers(){
+    public void returns_array_of_numbers_as_array_of_strings_with_numbers_or_empty_array_when_empty_array_is_received() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         assertArrayEquals(fizzBuzz.play(new int[] {1, 10, 20}), new String[] {"1", "10", "20"});
         assertArrayEquals(fizzBuzz.play(new int[] {}), new String[] {});
+    }
+
+
+    @Test
+    public void return_null_when_error() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertArrayEquals(fizzBuzz.play(new int[] {-3}), null);
+        assertArrayEquals(fizzBuzz.play(new int[] {4, -9}), null);
     }
 
 }
