bb1d32a94f9160ebee77490be3a807581c6737f0
Pablo <alu0101225364@ull.edu.es>
2022-03-28 17:26:03 +0100

    Second test in red

--- src/test/java/FizzBuzz/FizzBuzz_must.java
+++ src/test/java/FizzBuzz/FizzBuzz_must.java
@@ -15,4 +15,18 @@ public class FizzBuzz_must {
         assertEquals(fizzBuzz.play(4), "4");
         assertEquals(fizzBuzz.play(98), "98");
     }
+
+    @Test
+    void not_accept_negative_numbers_or_zero() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        NumberFormatException expectedException = new NumberFormatException("Número no aceptado");
+
+
+        assertEquals(fizzBuzz.play(0), expectedException.getMessage());
+        assertEquals(fizzBuzz.play(-1), expectedException.getMessage());
+        assertEquals(fizzBuzz.play(-100), expectedException.getMessage());
+        assertEquals(fizzBuzz.play(-5), expectedException.getMessage());
+        assertEquals(fizzBuzz.play(-3), expectedException.getMessage());
+        assertEquals(fizzBuzz.play(-20), expectedException.getMessage());
+    }
 }
