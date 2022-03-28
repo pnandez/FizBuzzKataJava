a31fd6f8fd26083c0ecba8b0452ba8d5daf75dcc
Pablo <alu0101225364@ull.edu.es>
2022-03-28 17:38:34 +0100

    Sixth test in red

--- src/test/java/FizzBuzz/FizzBuzz_must.java
+++ src/test/java/FizzBuzz/FizzBuzz_must.java
@@ -41,10 +41,27 @@ public class FizzBuzz_must {
     @Test
     void check_if_multiple_of_five(){
         FizzBuzz fizzBuzz = new FizzBuzz();
+
         assertEquals(fizzBuzz.play(5), "Buzz");
         assertEquals(fizzBuzz.play(20), "Buzz");
         assertEquals(fizzBuzz.play(10), "Buzz");
         assertEquals(fizzBuzz.play(35), "Buzz");
         assertEquals(fizzBuzz.play(100), "Buzz");
+    }
+
+    @Test
+    void check_if_multiple_of_three_and_five() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals(fizzBuzz.play(15), "FizzBuzz");
+        assertEquals(fizzBuzz.play(30), "FizzBuzz");
+    }
+
+    @Test
+    void check_if_contains_number_three() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals(fizzBuzz.play(13), "Fizz");
+        assertEquals(fizzBuzz.play(31), "Fizz");
     }
 }
