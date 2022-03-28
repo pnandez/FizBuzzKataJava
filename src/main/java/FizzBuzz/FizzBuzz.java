b23381113fedf0e7ed16f9cbb5bd6afc7c6d4cbb
Pablo <alu0101225364@ull.edu.es>
2022-03-28 17:31:17 +0100

    Third test in green

--- src/main/java/FizzBuzz/FizzBuzz.java
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -6,6 +6,10 @@ public class FizzBuzz {
     public String play(int number) {
         if(number <= 0 || number > 100)
             return "Número no aceptado";
+
+        if(number % 3 == 0)
+            return "Fizz";
+
         return Integer.toString(number);
     }
 }
